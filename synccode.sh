d7cdf889d3669820f72bc5ae15713329151b8611
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-07 22:05:34 +0800

    Updated DID app

--- synccode.sh
+++ synccode.sh
@@ -4,7 +4,7 @@ checkoutMaster() {
     git submodule foreach '
         if [ "$path" != "Dependency/SPV.Cpp" ]; then
             git checkout master
-	    git pull --rebase
+	        git pull --rebase
         fi'
 }
 
